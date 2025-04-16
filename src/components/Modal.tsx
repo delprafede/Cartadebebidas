182ab8158c5d3ec5deb6322f8a6f2572162d022a
delprafede <delpradofederico0@gmail.com>
2025-04-15 22:48:36 -0300

    modal terminado

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,31 @@
 // import { Dialog, Transition } from '@headlessui/react';
 // import { Fragment } from 'react';
+import { JSX } from "react";
 import { useAppStore } from "../stores/useAppStore";
 import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { SelectDrink } from "../types";
 
 const Modal = () => {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
+  const selectdDrink = useAppStore((state) => state.selectdDrink);
+
+  const renderIngredients = ()=> {
+    
+    const ingredients : JSX.Element[] = [];
+    for(let i=1; i <= 6; i++) {
+      const ingredient = selectdDrink[`strIngredient${i}` as keyof SelectDrink]
+      const measure = selectdDrink[`strMeasure${i}` as keyof SelectDrink]
+    if (ingredient && measure) {
+      ingredients.push(
+        <li key={i} className="flex items-center mb-2">
+          {ingredient} - {measure}
+        </li>
+      )
+    }
+    }
+    return ingredients
+  }
   return (
     <>
       <Dialog
@@ -19,68 +39,56 @@ const Modal = () => {
           <div className="flex min-h-full items-center justify-center p-4 bg-stone-500/40">
             <DialogPanel
               transition
-              className="w-full bg-amber-600 max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full  max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
-              <DialogTitle as="h3" className=" font-medium text-black text-2xl">
-                Payment successful
+              <DialogTitle
+                as="h3"
+                className=" font-medium text-black text-2xl text-center"
+              >
+                {selectdDrink.strDrink}
               </DialogTitle>
-              <p className="mt-2 text-lg/6 text-black">
-                Your payment has been successfully submitted. We’ve sent you an
-                email with all of the details of your order.
+              <div className=" p-2">
+                <img
+                  className=" w-96 mx-auto"
+                  src={selectdDrink.strDrinkThumb}
+                  alt={selectdDrink.strDrink}
+                />
+              </div>
+              <DialogTitle
+                as="h3"
+                className="  text-black text-2xl text-center font-extrabold my-5"
+              >
+                Ingredientes y Cantidades
+              </DialogTitle>
+              <p>
+              {renderIngredients()}
               </p>
-              <div className="mt-4">
+              <DialogTitle
+                as="h3"
+                className="  text-black text-2xl text-center
+                  font-extrabold my-5"
+              >
+                Instrucciones
+              </DialogTitle>
+              <p>{selectdDrink.strInstructions}</p>
+              <div className="mt-4 flex justify-between gap-4">
                 <Button
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                  className=" w-full items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
                   onClick={closeModal}
                 >
                   Cerrar
+                </Button>
+                <Button
+                  className=" w-full items-center gap-2 rounded-md bg-orange-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-orange-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                  // onClick={}
+                >
+                  Agregar a Favoritos
                 </Button>
               </div>
             </DialogPanel>
           </div>
         </div>
       </Dialog>
-      {/* <Transition appear show={modal} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-70" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
-                  <Dialog.Title as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
-                      Titulo Aquí
-                  </Dialog.Title>
-                  <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                    Ingredientes y Cantidades
-                  </Dialog.Title>
-                  <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                    Instrucciones
-                  </Dialog.Title>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition> */}
     </>
   );
 };
